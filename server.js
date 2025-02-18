53f653821e5d7cee22a9437cb8a3d46b265a71e8
zyingliew <zyingliew.vinceacademy@gmail.com>
2025-02-18 15:10:49 +0800

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http'); 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello, World! 服务器运行成功！');
-});
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`服务器运行在 http://localhost:${PORT}`);
-});
